c5c925b609d55578dd702a1a0f03aa232a4c71bf
Denozi Julien <denozi.j@gmail.com>
2021-05-13 01:38:22 +0200

    Minor chnage: update name function
    Signed-off-by: Denozi Julien <denozi.j@gmail.com>

--- src/cthulhu.py
+++ src/cthulhu.py
@@ -1,29 +1,43 @@
 from __future__ import annotations
 
+import base64
+import hashlib
+import hmac
 import json
 import logging
 import os
 import subprocess
 import sys
 import time
+import urllib
 
 import pandas as pd
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pandas import DataFrame
 from pip._vendor import requests
 
-KRAKEN_API_KEY = os.getenv("KRAKEN_API_PRIVATE_KEY", default="")
-KRAKEN_DOMAIN = "https://api.kraken.com{}"
-ASSETPAIRS = '/0/public/AssetPairs?pair={},{}?assetVersion=1'
+from src.asset import Asset
+from src.pair_asset import AssetPair
 
+load_dotenv()
+
+KRAKEN_API_PRIVATE_KEY = os.getenv("KRAKEN_API_PRIVATE_KEY", default="")
+KRAKEN_API_KEY = os.getenv("KRAKEN_API_KEY", default="")
+
+KRAKEN_URL =  "https://api.kraken.com"
+KRAKEN_DOMAIN = "https://api.kraken.com{}"
+ASSETPAIRS = "/0/public/AssetPairs?pair={}"
+TICKER_INFORMATION = "Ticker?pair={}"
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 class Cthulhu:
 
     def __init__(self):
         self.private_api_key = self._get_api_key()
         self.api_domain = KRAKEN_DOMAIN
-        self.asset = self._get_asset()
+        self.assets = self._get_assets()
         self.tradable_asset = self._get_tradable_asset()
 
     def _get_api_key(self):
@@ -60,7 +74,7 @@ class Cthulhu:
         else:
             return False
 
-    def _get_asset(self) -> pd.DataFrame:
+    def _get_assets(self) -> pd.DataFrame:
         """
         Allows you to retrieve information on assets
         :return:
