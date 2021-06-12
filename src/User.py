f2ec1a0566bb5a822cf36930ba2c08eced511ac0
Denozi Julien <denozi.j@gmail.com>
2021-06-13 00:07:49 +0200

    Add document string stub of preivous function

--- src/User.py
+++ src/User.py
@@ -11,13 +11,28 @@ class User:
         self.open_orders = cthulhu.get_open_orders()["result"]
         self.closed_orders = cthulhu.get_closed_orders()
 
-    def get_open_orders(self):
+    def get_open_orders(self) -> pd.Dataframe:
+        """
+        Get open order information
+        :return: Information of open order
+        """
         return pd.DataFrame(self.open_orders["open"]).transpose().reset_index().rename(columns={"index": "id"})
 
     def get_information_orders(self, ids_list: List[str]) -> json:
+        """
+        Get open order information of specific order
+        :param ids_list: List of ids
+        :return: Information of open order
+        """
         return pd.DataFrame(self.cthulhu.get_information_orders(ids_list)["result"]).transpose().reset_index().rename(columns={"index": "id"})
     
-    def get_prediction(self, asset_name_1: str, asset_name_2: str) -> pd.DataFrame:
+    def _get_prediction(self, asset_name_1: str, asset_name_2: str) -> pd.DataFrame:
+        """
+        Compute prediction currency of the asset given
+        :param asset_name_1: Asset name
+        :param asset_name_2: Asset name
+        :return: The dataframe of currency prediction
+        """
         asset = self.cthulhu.get_asset(asset_name_1)
         asset_2 = self.cthulhu.get_asset(asset_name_2)
         pair_asset = self.cthulhu.get_pair_asset(asset, asset_2)
