d1374049fd89d5590d0dcddc15f938f2a9af8aa2
Denozi Julien <denozi.j@gmail.com>
2021-06-12 22:58:41 +0200

    Add function to cast open informations dataframe into dataframe

--- src/User.py
+++ src/User.py
@@ -1,10 +1,15 @@
+import json
+from typing import List
 from src.cthulhu import Cthulhu
-
+import pandas as pd
 
 class User:
     def __init__(self, cthulhu: Cthulhu):
+        self.cthulhu = cthulhu
         self.balance = cthulhu.get_balance()
-        self.open_orders = cthulhu.get_open_orders()
+        self.open_orders = cthulhu.get_open_orders()["result"]
         self.closed_orders = cthulhu.get_closed_orders()
 
+    def get_open_orders(self):
+        return pd.DataFrame(self.open_orders["open"]).transpose()
 
