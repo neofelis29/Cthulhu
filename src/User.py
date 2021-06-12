bfa42a626bad8baa58c878b28ec5a313ff64a9fd
Denozi Julien <denozi.j@gmail.com>
2021-06-12 23:36:09 +0200

    Add function to get prediction from asset name

--- src/User.py
+++ src/User.py
@@ -16,3 +16,15 @@ class User:
 
     def get_information_orders(self, ids_list: List[str]) -> json:
         return pd.DataFrame(self.cthulhu.get_information_orders(ids_list)["result"]).transpose().reset_index().rename(columns={"index": "id"})
+    
+    def get_prediction(self, asset_name_1: str, asset_name_2: str) -> pd.DataFrame:
+        asset = self.cthulhu.get_asset(asset_name_1)
+        asset_2 = self.cthulhu.get_asset(asset_name_2)
+        pair_asset = self.cthulhu.get_pair_asset(asset, asset_2)
+        success, result = pair_asset.predict_crypto_currency(forecasting_time=7 * 24)
+        if success:
+            return result.iloc[:7*24]
+        else:
+            return None
+    
+
---
+++ test/test_user.py
@@ -0,0 +1,30 @@
+import unittest
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+from src.User import User
+from src.cthulhu import Cthulhu
+import warnings
+
+from src.utils import logger_init
+
+warnings.simplefilter("ignore", ResourceWarning)
+load_dotenv()
+
+KRAKEN_API_PRIVATE_KEY = os.getenv("KRAKEN_API_PRIVATE_KEY", default="")
+KRAKEN_API_KEY = os.getenv("KRAKEN_API_KEY", default="")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestVastAPI(unittest.TestCase):
+    def setUp(self):
+        logger_init()
+        self.cthulhu = Cthulhu()
+        self.user = User(self.cthulhu)
+
+    def test_prediction(self):
+        pred = self.user.get_prediction("Energy Web Token", "Euro")
+        self.assertIsNotNone(pred)
